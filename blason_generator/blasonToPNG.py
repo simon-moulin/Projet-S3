3fd06cc96f64bae28385f450e1a6a001d2f477c4
Sylvain Reynaud <sylvainreynaud@protonmail.com>
2019-10-16 15:43:00 +0200

    blasons_png.txt -> id;nom

--- blason_generator/blasonToPNG.py
+++ blason_generator/blasonToPNG.py
@@ -13,18 +13,23 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get(url)
 
 f = open("blasons_png.txt", 'a')
-i=2340
-while i<=6000:
-    i=i+1
-    img_b64 = WebDriverWait(driver, 20).until(
-        lambda driver: driver.find_element_by_css_selector('img.device-preview').get_attribute('ng-src'))[22:]
+i = 11638
+while i <= 15000:
+    i = i+1
+    while True:
+        try:
+            img_b64 = WebDriverWait(driver, 20).until(
+                lambda driver: driver.find_element_by_css_selector('img.device-preview').get_attribute('ng-src'))[22:]
+            break
+        except:
+            pass
     nom = driver.find_element_by_css_selector('em.ng-binding').get_attribute('innerHTML')[1:-1]
 
     im = Image.open(BytesIO(base64.b64decode(img_b64)))
     im.save("images/"+str(i)+".png", 'PNG')
-    f.write(nom +"\n")
+    f.write(str(i) + ';' + nom + '\n')
     print(nom)
-    print(str(i)+ "\n")
+    print(str(i) + "\n")
 
     while True:
         driver.find_element_by_class_name('brass-button-small').click()
